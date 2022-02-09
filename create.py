656a181d863224f6b72db3b865efb375129fa099
Siddhartha Bose <siddbose97@gmail.com>
2022-02-09 19:57:32 +0800

    trying to add db

--- create.py
+++ create.py
@@ -28,7 +28,8 @@ def checkpw(update_obj, context):
         msg = update_obj.message.text
         pw = "admin62"
         if msg == pw:
-            update_obj.message.reply_text("Correct PW! Please type in name of checklist")
+            # update_obj.message.reply_text("Correct PW! Please type in name of checklist")
+            update_obj.message.reply_text(mongo_string)
             return NAMECL
         else:
             update_obj.message.reply_text("SORRY INCORRECT PW")
@@ -42,17 +43,17 @@ def namecl(update_obj, context):
 
         db = client.checklists
         active_checklists = db.active_checklists
-        result = active_checklists.insert_one({"checklist name":msg})
 
         update_obj.message.reply_text(f"Your new checklist is named {msg}. Please ask depot to access this checklist")
+        result = active_checklists.insert_one({"checklist name":msg})
+
         return ConversationHandler.END
     except Exception as e:
         cancel(e, context)
 
 def cancel(update_obj, context):
     # get the user's first name
-    first_name = update_obj.message.from_user['first_name']
     update_obj.message.reply_text(
-        f"Okay, no question for you then, take care, {first_name}! Please click /start to start again",\
+        f"Okay, no question for you then, take care! Please click /start to start again",\
              reply_markup=telegram.ReplyKeyboardRemove())
     return ConversationHandler.END
