34b8a8ade83121f9da626797bbd00a019ee9bd23
Siddhartha Bose <siddbose97@gmail.com>
2022-02-10 19:54:14 +0800

    check.py started

--- check.py
+++ check.py
@@ -12,25 +12,26 @@ def checkcl(update_obj, context):
     try:
         load_dotenv()
         mongo_string = str(os.getenv('MONGO_STRING'))
-        msg = update_obj.message.text
         client = MongoClient(mongo_string)
-
+        print("here1")
         db = client.checklists
         active_checklists = db.active_checklists
         arr_of_checklists = []
         is_list_query = {"is_list":"yes"}
         cursor = active_checklists.find(is_list_query)
-
+        print("here2")
         for checklist in cursor:
             arr_of_checklists.append(checklist.name)
-
+        print("here3")
         if arr_of_checklists:
+            print("here4")
             list1 = [[telegram.KeyboardButton(text=checklist_name)] for checklist_name in arr_of_checklists]
             kb = telegram.ReplyKeyboardMarkup(keyboard=list1,resize_keyboard = True, one_time_keyboard = True)
 
             update_obj.message.reply_text("Please choose a checklist from the following active lists",reply_markup=kb)
             return PRINTCL
         else:
+            print("here5")
             update_obj.message.reply_text("There are no active checklists at the moment")
             return ConversationHandler.END
     except Exception as e:
